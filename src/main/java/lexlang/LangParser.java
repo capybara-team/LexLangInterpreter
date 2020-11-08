5bef6d10ed7afc32f6f9484a69310d018e242160
Rodolpho Rossete <rodolpho@ice.ufjf.br>
2020-11-07 22:05:54 -0300

    Criação do parser e do fluxo main

---
+++ src/main/java/lexlang/LangParser.java
@@ -0,0 +1,34 @@
+package lexlang;
+
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.File;
+
+public class LangParser {
+
+    public static void interpreterFile(String path ){
+        File file  = new File(path);
+        try{
+            if( file.exists() ){
+                System.out.println("Executando: " + file.getPath());
+
+
+                LexLangLexer lexer = new LexLangLexer(new ANTLRFileStream(file.getPath()));
+                LexLangParser parser = new LexLangParser(new CommonTokenStream(lexer));
+                ParseTree tree =  parser.prog();
+                Visitor visitor = new Visitor();
+                System.out.println(tree.getText());
+                visitor.visit(tree);
+
+            }else{
+                System.out.println("O caminho " + file.getPath() + " não existe.");
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/lexlang/Main.java
+++ src/main/java/lexlang/Main.java
@@ -3,8 +3,16 @@ package lexlang;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Ola mundo");
+		if (args.length < 1) {
+			System.out.println("Lang compiler V0.0.1 - Outubro de 2020");
+			System.out.println("Ação: ");
+			System.out
+					.println(" <caminho_arquivo> : Interpreta um arquivo específico.");
+
+		}
+		if (args.length == 1){
+			LangParser.interpreterFile(args[0]);
+		}
 
 	}
 
